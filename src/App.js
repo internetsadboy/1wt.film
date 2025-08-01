99b49b75bb102f08954d567efb012208bcabf52d
isb <jaredlamont90@gmail.com>
2025-08-01 11:53:29 -0700

    added: bare bones html 1wt

--- src/App.js
+++ src/App.js
@@ -3,9 +3,101 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">      
-        <p>this is the website for one way tix</p>
-      </header>
+      <section id="yellow-bar"></section>
+      <section id="main-header">
+        <h1>
+          ONE WAY TIX
+        </h1>
+        <p className="subtitle">A documentary feature film, <span className="bold">10 years</span> in the making.</p>
+      </section>
+
+      <section>
+        <h4>SYNOPSIS</h4>
+        <p>
+          A guy buys a one way ticket to Asia
+          <span className="bold">determined to make a documentary</span>
+          with no money, no experience, and no direction. He attempts to show that
+          travel is accesible by
+          <span className="bold">working for food and shelter</span> but his obsession surfaces; being <span className="bold">on the run from materialism.</span>The expedition ends near the Siberian border herding cattle. Is the documentary about Jenny? The Monks? Angela, the dead piglet? Obama, the unriddeable horse in Mongolia? The Deadliest Catch boat? Mysterious characters
+          appear and guide him along the way. Overwhelmed, he burries the project and accepts defeat. <span className="bold">Six years later</span> he learns that <span className="bold">Asia was only the beginning</span> of becoming a documentary filmmaker.
+        </p>
+      </section>
+
+      <section>
+        <h4>COUNTRIES</h4>
+        <p style={{fontFamily: "Consolas, monospace", fontSize: "14px"}}>
+          <span className="highlight-alt">FRANCE</span>,
+          <span className="highlight-alt">US</span>,
+          <span className="highlight-alt">INDONESIA</span>,
+          <span className="highlight-alt">MALAYSIA</span>,
+          <span className="highlight-alt">VIET NAM</span>,
+          <span className="highlight-alt">HONG KONG</span>,
+          <span className="highlight-alt">NEPAL</span>,
+          <span className="highlight-alt">"TIBET"</span>,
+          <span className="highlight-alt">CHINA</span>,
+          <span className="highlight-alt">MONGOLIA</span>
+        </p>
+      </section>
+
+      <section>
+        <h4>TRAILER</h4>
+        <div style={{padding: "56.25% 0 0 0", position: "relative"}}>
+          <iframe
+            src="https://player.vimeo.com/video/1083081464?title=0&byline=0&portrait=0&badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+            frameBorder="0"
+            allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media"
+            style={{position: "absolute", top: 0, left: 0, width: "100%", height: "100%"}}
+            title="ONE WAY TIX - Unofficial Trailer"
+          ></iframe>
+        </div>
+        <script src="https://player.vimeo.com/api/player.js"></script>
+      </section>
+
+      <section>
+        <h4>THEMES</h4>
+        <ul>
+          <li>Escaping Materialism</li>
+          <li>Animal Consumption</li>
+          <li>Self-Discovery</li>
+          <li>Manifestation</li>
+          <li>Friendship</li>
+          <li>Adventure</li>
+          <li>Death</li>
+          <li>Self-Sabotage</li>
+          <li>Mental Health</li>
+        </ul>
+      </section>
+
+      <section>
+        <p>
+          <span className="highlight"> ANTICIPATED RELEASE </span>
+          <span className="highlight-alt">Jan 22, 2026.</span>
+        </p>
+        <p>
+          Now in <span className="italic bold">post-production</span>, One Way Tix is
+          being submitted to major festivals worldwide.
+        </p>
+      </section>
+
+      <section>
+        THIS PROJECT CAN BE DESCRIBED AS
+        <span className="bold highlight-alt"> GUERILLA FILMMAKING.</span>
+      </section>
+
+      <section>
+        <p>Filming began in November 2015, unknowingly.</p>
+      </section>
+
+      <div className="form-container">
+        <h2 className="form-header highlight">Get notified when the film is released!</h2>
+        <form className="form">
+          <input type="email" placeholder="Enter your email" className="email-input" />
+          <button type="submit" className="submit-button">Submit</button>
+        </form>
+      </div>   
+      <section>
+        <span ><a className="highlight-alt" style={{fontWeight: 400}} href="#">contact@onewaytix.film</a></span>
+      </section>
     </div>
   );
 }
